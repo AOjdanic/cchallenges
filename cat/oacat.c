b41bb61fb54caecc4b75992efbe6856ddb20a967
AOjdanic <aojdanic97@gmail.com>
2024-11-17 11:06:24 +0100

    read from stdin if we have no arguments or the argument is a "-"

--- cat/oacat.c
+++ cat/oacat.c
@@ -5,6 +5,15 @@
 int main(int argc, char **argv) {
   FILE *p_input_file;
   int character;
+
+  if (*(argv + 1) == NULL || strstr(*(argv + 1), "-")) {
+    while ((character = getc(stdin)) != EOF) {
+      putchar(character);
+    }
+    if (EOF)
+      return 0;
+  }
+
   p_input_file = fopen(*(argv + 1), "r");
   assert(p_input_file);
 
