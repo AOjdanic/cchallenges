fa1ef9ec57089b250d77849dce47b58d9848279c
AOjdanic <aojdanic97@gmail.com>
2024-11-17 14:27:36 +0100

    enable concatenating files

--- cat/oacat.c
+++ cat/oacat.c
@@ -1,25 +1,41 @@
-#include <assert.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char **argv) {
   FILE *p_input_file;
   int character;
 
-  if (*(argv + 1) == NULL || strstr(*(argv + 1), "-")) {
+  if (argc == 1) {
     while ((character = getc(stdin)) != EOF) {
       putchar(character);
     }
     if (EOF)
-      return 0;
+      exit(0);
   }
 
-  p_input_file = fopen(*(argv + 1), "r");
-  assert(p_input_file);
+  while ((*++argv) != NULL) {
+    printf("--------------the pointer value in while loop: %s-------------\n",
+           *argv);
+    if (**argv == '-') {
+      while ((character = getchar()) != EOF) {
+        putchar(character);
+      }
+      if (EOF)
+        continue;
+    }
 
-  while ((character = getc(p_input_file)) != EOF) {
-    putchar(character);
+    p_input_file = fopen(*(argv), "r");
+
+    if (!p_input_file) {
+      printf("Error: can\'t open the specified file: %s\n", *argv);
+      exit(1);
+    }
+
+    while ((character = getc(p_input_file)) != EOF) {
+      putchar(character);
+    }
+    fclose(p_input_file);
   }
 
-  return 0;
+  exit(0);
 }
