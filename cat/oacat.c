582f8a926a76f3e7928743377bf6044810d79bbc
AOjdanic <aojdanic97@gmail.com>
2024-11-19 00:59:37 +0100

    wip: create helper functions

--- cat/oacat.c
+++ cat/oacat.c
@@ -1,93 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct flags {
+struct options {
   int number_all_lines;
   int ends_of_lines;
   int squeeze_non_empty_lines;
   int number_non_empty_lines;
 };
 
+FILE *p_input_file;
+int character;
+int number_of_lines = 1;
+int position_in_arguments = 0;
+struct options options = {0, 0, 0, 0};
+
+void set_program_options(char **argv, struct options *options) {
+  while (*++(*argv)) {
+    char current_character = **argv;
+    printf("current character: %c\n", current_character);
+    if (current_character == 'b') {
+      // b overrides n
+      options->number_non_empty_lines = 1;
+      options->number_all_lines = 0;
+    }
+
+    if (current_character == 'e')
+      options->ends_of_lines = 1;
+
+    if (current_character == 'n' && options->number_non_empty_lines == 0)
+      options->number_all_lines = 1;
+
+    if (current_character == 'h')
+      printf("CURRENT CHARACTER IS h\n");
+
+    if (current_character == 's')
+      options->squeeze_non_empty_lines = 1;
+  }
+}
+
+void add_filename_to_file_list(char **files, char **argv,
+                               int *position_in_arguments) {
+  files[*position_in_arguments] = *argv;
+  (*position_in_arguments)++;
+};
+
+void copy_input_to_output(FILE *p_input_file, FILE *p_output_file) {
+  while ((character = getc(p_input_file)) != EOF) {
+    putc(character, p_output_file);
+  }
+}
 int main(int argc, char **argv) {
-  FILE *p_input_file;
-  int character;
-  int number_of_lines = 1;
-  struct flags flags = {0, 0, 0, 0};
-  char **file_arguments = (char **)malloc(argc * sizeof(char *));
-  int position_in_arguments = 0;
+  char **files = malloc(argc * sizeof(char *));
 
   if (argc == 1) {
-    while ((character = getc(stdin)) != EOF) {
-      putchar(character);
-    }
+    copy_input_to_output(stdin, stdout);
     exit(0);
   }
 
   while (--argc > 0) {
     ++argv;
     char *address_of_current_character = *argv;
-    if (**argv == '-' && ((*++(address_of_current_character)) != '\0')) {
-      while (*++(*argv)) {
-        char current_character = **argv;
-        printf("current character: %c\n", current_character);
-        if (current_character == 'b') {
-          // b overrides n
-          flags.number_non_empty_lines = 1;
-          flags.number_all_lines = 0;
-          printf("CURRENT CHARACTER IS b\n");
-        }
-        if (current_character == 'e') {
-          flags.ends_of_lines = 1;
-          printf("CURRENT CHARACTER IS e\n");
-        }
-        if (current_character == 'n') {
-          if (flags.number_non_empty_lines == 0) {
-            flags.number_all_lines = 1;
-          }
-          printf("CURRENT CHARACTER IS n\n");
-        }
-        if (current_character == 'h') {
-          // print help/usage
-          printf("CURRENT CHARACTER IS h\n");
-        }
-        if (current_character == 's') {
-          flags.squeeze_non_empty_lines = 1;
-          printf("CURRENT CHARACTER IS s\n");
-        }
-      }
-    } else {
-      file_arguments[position_in_arguments] = *argv;
-      position_in_arguments++;
-    }
+    int is_an_option_argument =
+        **argv == '-' && ((*++(address_of_current_character)) != '\0');
+
+    if (is_an_option_argument)
+      set_program_options(argv, &options);
+    else
+      add_filename_to_file_list(files, argv, &position_in_arguments);
   }
 
-  printf("%d, %d, %d, %d\n", flags.squeeze_non_empty_lines,
-         flags.number_non_empty_lines, flags.number_all_lines,
-         flags.ends_of_lines);
+  printf("%d, %d, %d, %d\n", options.squeeze_non_empty_lines,
+         options.number_non_empty_lines, options.number_all_lines,
+         options.ends_of_lines);
 
-  while (*file_arguments) {
+  while (*files) {
+    int filename_is_minus = **files == '-' && (*++(*files) == '\0');
 
-    if (**file_arguments == '-' && (*++(*file_arguments) == '\0')) {
-      while ((character = getc(stdin)) != EOF) {
-        putchar(character);
-      }
-    } else {
-      p_input_file = fopen(*(file_arguments), "r");
+    if (filename_is_minus)
+      copy_input_to_output(stdin, stdout);
+    else {
+      p_input_file = fopen(*files, "r");
 
       if (!p_input_file) {
-        printf("Error: can\'t open the specified file: %s\n", *file_arguments);
+        printf("Error: can\'t open the specified file: %s\n", *files);
         exit(1);
       }
 
-      while ((character = getc(p_input_file)) != EOF) {
-        putchar(character);
-      }
+      copy_input_to_output(p_input_file, stdout);
       fclose(p_input_file);
     }
 
-    file_arguments++;
+    files++;
   }
 
-  free(file_arguments);
+  free(*files);
   exit(0);
 }
