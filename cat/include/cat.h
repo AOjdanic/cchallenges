4f54b927b44e4a11f78954e415271a0574cd48bf
AOjdanic <aojdanic97@gmail.com>
2024-11-24 02:57:52 +0100

    prevent inclusion of cat.h file more than once | create a help macro for printing when user enters -h option

--- cat/include/cat.h
+++ cat/include/cat.h
@@ -1,3 +1,5 @@
+#ifndef CAT_H
+#define CAT_H
 #include <stdbool.h>
 #include <stdio.h>
 
@@ -16,3 +18,19 @@ void add_file(char **files, char **argv);
 void set_options(char **argv, struct options *options);
 void pipe_io(FILE *p_input_file, struct options *options, char **input_lines);
 void pipe_file_io(char **p_files, struct options *options, char **input_lines);
+
+#define HELP                                                                   \
+  "Usage: oacat [OPTION]... [FILE]... \n"                                      \
+  "Concatenate FILE(s) to standard output.\n"                                  \
+  "With no FILE, or when FILE is -, read standard input.\n"                    \
+  "  -b                       number nonempty output lines, overrides -n\n"    \
+  "  -e                       display $ at end of each line\n"                 \
+  "  -n                       number all output lines\n"                       \
+  "  -s                       suppress repeated empty output lines\n"          \
+  "  -h                       display this help and exit\n"                    \
+  "Examples:\n"                                                                \
+  "  oacat file1 - file2  Output file1's contents, then standard input, then " \
+  "file2's contents.\n"                                                        \
+  "  oacat        Copy standard input to standard output.\n"
+
+#endif /* CAT_H */
--- cat/src/set_options.c
+++ cat/src/set_options.c
@@ -1,25 +1,28 @@
 #include "../include/cat.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 void set_options(char **argv, struct options *options) {
   while (*++(*argv)) {
     char current_character = **argv;
 
     if (current_character == 'b') {
-      // b overrides n
       options->enumerate_lines = 1;
-      options->enumerate_all_lines = 0;
-    }
-
-    if (current_character == 'e')
+      options->enumerate_all_lines = 0; // b overrides n
+    } else if (current_character == 'e') {
       options->mark_ends = 1;
-
-    if (current_character == 'n' && options->enumerate_lines == 0)
+    } else if (current_character == 'n' && options->enumerate_lines == 0) {
       options->enumerate_all_lines = 1;
-
-    if (current_character == 'h')
-      printf("CURRENT CHARACTER IS h\n");
-
-    if (current_character == 's')
+    } else if (current_character == 'h') {
+      printf(HELP);
+      exit(0);
+    } else if (current_character == 's') {
       options->squeeze_lines = 1;
+    } else {
+      printf(
+        "Invalid option provided: -%c. Type oacat -h to get tips on usage\n",
+        current_character);
+      exit(1);
+    }
   }
 }
