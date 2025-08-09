e5672e158e0ec0e461f9962b68f4d2c5700e8858
Aleksa Ojdanic <aojdanic97@gmail.com>
2025-08-09 22:54:39 +0200

    a small C implementation of a dynamic array data structure

---
+++ dsa/dynamic_array.c
@@ -0,0 +1,147 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct dynamic_array {
+  int size;
+  int capacity;
+  int *elements;
+} DynamicArray;
+
+DynamicArray *DynamicArray_initialize(int initialSize) {
+  int size = initialSize ? initialSize : 1;
+
+  int *p = malloc(size * sizeof(int));
+  if (p == NULL) {
+    return NULL;
+  }
+
+  DynamicArray *arr = malloc(sizeof(DynamicArray));
+
+  if (arr == NULL) {
+    free(p);
+    return NULL;
+  }
+
+  arr->size = 0;
+  arr->capacity = size;
+  arr->elements = p;
+
+  return arr;
+}
+
+void DynamicArray_free(DynamicArray **arr) {
+  if (!arr || !*arr) {
+    return;
+  }
+
+  DynamicArray *tmp = *arr;
+
+  free(tmp->elements);
+  tmp->elements = NULL; // do this to avoid dangling pointer
+
+  free(tmp);
+  *arr = NULL;
+}
+
+DynamicArray *DynamicArray_push(DynamicArray *arr, int el) {
+  if (!arr || !arr->elements) {
+    return NULL;
+  }
+
+  if (arr->size == arr->capacity) {
+    int *newP = realloc(arr->elements, arr->capacity * 2 * sizeof(int));
+    if (newP == NULL) {
+      // improve this with some error
+      return arr;
+    }
+
+    arr->elements = newP;
+    arr->capacity *= 2;
+  }
+
+  arr->elements[arr->size] = el;
+  arr->size++;
+
+  return arr;
+}
+
+int DynamicArray_pop(DynamicArray *arr) {
+  // i originally wrote arr->elements, instead of size, which is wrong,
+  // because then im checking whether a pointer is null, and not whether
+  // the array is empty
+  if (arr->size == 0) {
+    // improve this with error
+    return 0;
+  }
+  int temp = arr->elements[arr->size - 1];
+  arr->size--;
+
+  // here we can return the popped value, but we should not return a
+  // pointer to it, because if the array gets resized, or the value gets
+  // overwritten, we may get a dangling pointer, or a pointer that points to
+  // incorrect data
+  return temp;
+}
+
+int DynamicArray_remove(DynamicArray *arr, int index) {
+  if (!arr || !arr->elements || index < 0 || index >= arr->size) {
+    return 0;
+  }
+  int removed = arr->elements[index];
+
+  if (index == arr->size - 1) {
+    return DynamicArray_pop(arr);
+  }
+
+  if (index < arr->size - 1) {
+    memmove(&arr->elements[index], &arr->elements[index + 1],
+            (arr->size - index - 1) * sizeof(int));
+  }
+
+  arr->size--;
+
+  return removed;
+}
+
+int *DynamicArray_get(DynamicArray *arr, int index) {
+  if (!arr || !arr->elements || index < 0 || index >= arr->size) {
+    return NULL;
+  }
+
+  return &arr->elements[index];
+}
+
+void DynamicArray_set(DynamicArray *arr, int index, int value) {
+  if (!arr || !arr->elements || index < 0 || index >= arr->size) {
+    return;
+  }
+
+  arr->elements[index] = value;
+}
+
+int main() {
+  DynamicArray *arr = DynamicArray_initialize(0);
+
+  for (int i = 0; i < 9; i++) {
+    DynamicArray_push(arr, i);
+    printf("%d ", *DynamicArray_get(arr, i));
+  }
+
+  printf("\n");
+
+  DynamicArray_set(arr, 2, 10);
+  DynamicArray_pop(arr);
+  DynamicArray_remove(arr, 5);
+
+  for (int i = 0; i < arr->size; i++) {
+    printf("%d ", *DynamicArray_get(arr, i));
+  }
+
+  DynamicArray_free(&arr);
+
+  printf("\n");
+}
+
+// improvements
+// error handling
