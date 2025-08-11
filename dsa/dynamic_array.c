ae19c7c7cf128dae1336bb730270071f74a6c6d9
Aleksa Ojdanic <aojdanic97@gmail.com>
2025-08-12 01:26:39 +0200

    make dynamic arr generic

--- dsa/dynamic_array.c
+++ dsa/dynamic_array.c
@@ -6,13 +6,14 @@
 typedef struct dynamic_array {
   int size;
   int capacity;
-  int *elements;
+  void *elements;
+  size_t elementSize;
 } DynamicArray;
 
 void DynamicArray_shrink(DynamicArray *arr) {
   if (arr->size < arr->capacity / 4 && arr->capacity > 1) {
     int newCap = arr->capacity / 2;
-    int *newP = realloc(arr->elements, newCap * sizeof(int));
+    void *newP = realloc(arr->elements, newCap * arr->elementSize);
     if (newP) {
       arr->elements = newP;
       arr->capacity = newCap;
@@ -20,24 +21,29 @@ void DynamicArray_shrink(DynamicArray *arr) {
   }
 }
 
-DynamicArray *DynamicArray_initialize(int initialSize) {
-  int size = initialSize ? initialSize : 1;
+DynamicArray *DynamicArray_initialize(size_t elementSize, int initialCapacity) {
+  if (elementSize == 0) {
+    return NULL;
+  }
 
-  int *p = calloc(size, sizeof(int));
-  if (p == NULL) {
+  int capacity = initialCapacity > 0 ? initialCapacity : 1;
+
+  void *p = calloc(capacity, elementSize);
+  if (!p) {
     return NULL;
   }
 
   DynamicArray *arr = malloc(sizeof(DynamicArray));
 
-  if (arr == NULL) {
+  if (!arr) {
     free(p);
     return NULL;
   }
 
   arr->size = 0;
-  arr->capacity = size;
   arr->elements = p;
+  arr->capacity = capacity;
+  arr->elementSize = elementSize;
 
   return arr;
 }
@@ -56,37 +62,35 @@ void DynamicArray_free(DynamicArray **arr) {
   *arr = NULL;
 }
 
-bool DynamicArray_push(DynamicArray *arr, int el) {
-  if (!arr || !arr->elements || arr->size > arr->capacity) {
+bool DynamicArray_push(DynamicArray *arr, void *el) {
+  if (!arr || !arr->elements) {
     return false;
   }
 
-  if (arr->capacity == 0) {
-    arr->capacity = 1;
-  }
-
   if (arr->size == arr->capacity) {
-    int *newP = realloc(arr->elements, arr->capacity * 2 * sizeof(int));
-    if (newP == NULL) {
-      return NULL;
+    void *newP = realloc(arr->elements, arr->capacity * 2 * arr->elementSize);
+    if (!newP) {
+      return false;
     }
 
     arr->elements = newP;
     arr->capacity *= 2;
   }
 
-  arr->elements[arr->size] = el;
+  void *dest = (char *)arr->elements + arr->size * arr->elementSize;
+  memcpy(dest, el, arr->elementSize);
   arr->size++;
 
   return true;
 }
 
-bool DynamicArray_pop(DynamicArray *arr, int *result) {
+bool DynamicArray_pop(DynamicArray *arr, void *out) {
   if (!arr || !arr->elements || !arr->size) {
     return false;
   }
 
-  *result = arr->elements[arr->size - 1];
+  void *src = (char *)arr->elements + (arr->size - 1) * arr->elementSize;
+  memcpy(out, src, arr->elementSize);
   arr->size--;
 
   DynamicArray_shrink(arr);
@@ -94,20 +98,22 @@ bool DynamicArray_pop(DynamicArray *arr, int *result) {
   return true;
 }
 
-bool DynamicArray_remove(DynamicArray *arr, int index, int *result) {
+bool DynamicArray_remove(DynamicArray *arr, int index, void *out) {
   if (!arr || !arr->elements || index < 0 || index >= arr->size) {
     return false;
   }
 
   if (index == arr->size - 1) {
-    return DynamicArray_pop(arr, result);
+    return DynamicArray_pop(arr, out);
   }
 
-  *result = arr->elements[index];
+  void *src = (char *)arr->elements + index * arr->elementSize;
+  memcpy(out, src, arr->elementSize);
 
   if (index < arr->size - 1) {
-    memmove(&arr->elements[index], &arr->elements[index + 1],
-            (arr->size - index - 1) * sizeof(int));
+    void *dst = (char *)arr->elements + index * arr->elementSize;
+    void *src = (char *)arr->elements + (index + 1) * arr->elementSize;
+    memmove(dst, src, (arr->size - index - 1) * arr->elementSize);
   }
 
   arr->size--;
@@ -116,58 +122,64 @@ bool DynamicArray_remove(DynamicArray *arr, int index, int *result) {
   return true;
 }
 
-bool DynamicArray_get(DynamicArray *arr, int index, int *result) {
+bool DynamicArray_get(DynamicArray *arr, int index, void *out) {
   if (!arr || !arr->elements || index < 0 || index >= arr->size) {
     return false;
   }
 
-  *result = arr->elements[index];
+  void *src = (char *)arr->elements + index * arr->elementSize;
+  memcpy(out, src, arr->elementSize);
 
   return true;
 }
 
-bool DynamicArray_set(DynamicArray *arr, int index, int value) {
+bool DynamicArray_set(DynamicArray *arr, int index, void *value) {
   if (!arr || !arr->elements || index < 0 || index >= arr->size) {
     return false;
   }
 
-  arr->elements[index] = value;
+  void *dest = (char *)arr->elements + index * arr->elementSize;
+  memcpy(dest, value, arr->elementSize);
 
   return true;
 }
 
 int main() {
-  DynamicArray *arr = DynamicArray_initialize(0);
+  DynamicArray *arr = DynamicArray_initialize(sizeof(int), 0);
 
-  for (int i = 0; i < 9; i++) {
-    DynamicArray_push(arr, i);
-    int value;
-    if (DynamicArray_get(arr, i, &value)) {
-      printf("%d ", value);
+  if (arr) {
+    for (int i = 0; i < 9; i++) {
+      DynamicArray_push(arr, &i);
+      int value;
+      if (DynamicArray_get(arr, i, &value)) {
+        printf("%d ", value);
+      }
     }
-  }
 
-  printf("\n");
+    printf("\n");
 
-  DynamicArray_set(arr, 2, 10);
-  int poppedValue;
-  if (DynamicArray_pop(arr, &poppedValue)) {
-    printf("poppedValue: %d\n", poppedValue);
-  }
+    int setter = 10;
 
-  int removed;
-  if (DynamicArray_remove(arr, 5, &removed)) {
-    printf("removed: %d\n", removed);
-  }
-
-  for (int i = 0; i < arr->size; i++) {
-    int value;
-    if (DynamicArray_get(arr, i, &value)) {
-      printf("%d ", value);
+    DynamicArray_set(arr, 2, &setter);
+    int poppedValue;
+    if (DynamicArray_pop(arr, &poppedValue)) {
+      printf("poppedValue: %d\n", poppedValue);
     }
+
+    int removed;
+    if (DynamicArray_remove(arr, 5, &removed)) {
+      printf("removed: %d\n", removed);
+    }
+
+    for (int i = 0; i < arr->size; i++) {
+      int value;
+      if (DynamicArray_get(arr, i, &value)) {
+        printf("%d ", value);
+      }
+    }
+
+    DynamicArray_free(&arr);
+
+    printf("\n");
   }
-
-  DynamicArray_free(&arr);
-
-  printf("\n");
 }
